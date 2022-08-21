f9168e27658f65a2d77d787b9821465811f35459
Mikhail Demashkevich <demashkevich@gmail.com>
2022-08-21 20:45:05 +0300

    add Text

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ const app = new Application({
   height: 500,
   transparent: false,
   antialias: true,
+  backgroundColor: 0x23395d,
 });
-
-app.renderer.backgroundColor = 0x23395d;
-
 document.body.appendChild(app.view);
 
 const wheel = PIXI.Sprite.from('./images/wheel.png');
@@ -21,6 +19,9 @@ const stopper = PIXI.Sprite.from('./images/stopper.png');
 stopper.x = app.screen.width / 2 - 15.5; // изменить 15.5 на что-то поумнее
 stopper.y = app.screen.height / 2;
 app.stage.addChild(stopper);
+
+const loader = PIXI.Loader.shared;
+loader.add(['./fonts/font_wheel.fnt']).load(createText);
 
 function sectorAngle(n) {
   if (n >= 1 && n <= 9) {
@@ -52,4 +53,27 @@ wheel.start = function (sectorNumber) {
       // ease: 'elastic',
     }
   );
+  textObj.text(111);
 };
+
+function createText() {
+  let textObj = new PIXI.Text(19, {
+    fontName: 'font_wheel',
+    // fontSize: 70,
+    // align: 'center',
+  });
+  textObj.anchor.set(0.5, 0.5);
+  textObj.x = app.screen.width / 2;
+  textObj.y = app.screen.height / 2;
+  app.stage.addChild(textObj);
+  // app.ticker.add(function (delta) {
+  //   text.text = Number(text.text) + 1;
+  // });
+  textObj.interactive = true;
+  return textObj;
+}
+
+// const setNumber = (value) => {
+//   number = value;
+//   textObj.setText(number);
+// };
