372faa3af7bcdb131fcde977eca84399656e2ff6
Mikhail Demashkevich <demashkevich@gmail.com>
2023-01-12 21:05:20 +0300

    first commit

--- Wheel.js
+++ Wheel.js
@@ -44,38 +44,3 @@ class Wheel {
     this.target_angle = angle_per_sector * sector_index;
   }
 }
-
-// class Wheel extends Scene {
-//   constructor() {
-//     super();
-//   }
-//   start() {
-//     this.getTargetAngle();
-
-//     gsap.to(this.wheel, {
-//       pixi: { rotation: 360 * 4 - this.target_angle },
-//       duration: 5,
-//       // ease: 'elastic',
-//       // ease: 'circ.out',
-//       // onUpdate: function () {
-//       //   let currentAngle =
-//       //     (gsap.getProperty(app.scene.loader.wheel, 'rotation') * 180) /
-//       //     Math.PI;
-//       //   currentAngle = currentAngle - Math.floor(currentAngle / 360) * 360;
-//       //   let currentSector =
-//       //     WHEEL_ANGLE.indexOf(
-//       //       WHEEL_ANGLE.reduce(function (a, c) {
-//       //         return Math.abs(a - currentAngle) < Math.abs(c - currentAngle)
-//       //           ? a
-//       //           : c;
-//       //       })
-//       //     ) + 1;
-//     });
-//   }
-//   getTargetAngle() {
-//     let angle_per_sector = 360 / SECTORS.length;
-//     // console.log(angle_per_sector);
-//     let sector_index = SECTORS.indexOf(this.sectorNumber);
-//     this.target_angle = angle_per_sector * sector_index;
-//   }
-// }
